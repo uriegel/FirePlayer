666e85e4a5f66b9e36449e47be541f0ee0d31e20
Uwe Riegel <uriegel@hotmail.de>
2020-05-09 17:03:52 +0200

    PlayerActivity and theming

--- app/src/main/java/de/uriegel/fireplayer/PlayerActivity.kt
+++ app/src/main/java/de/uriegel/fireplayer/PlayerActivity.kt
@@ -1,28 +1,21 @@
 package de.uriegel.fireplayer
 
-import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
-import androidx.appcompat.app.AppCompatActivity
-import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_player.*
 
-
-class MainActivity : AppCompatActivity() {
+class PlayerActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_player)
 
-//        uiContainer.systemUiVisibility = (View.SYSTEM_UI_FLAG_LOW_PROFILE
+//        playerContainer.systemUiVisibility = (View.SYSTEM_UI_FLAG_LOW_PROFILE
 //                or View.SYSTEM_UI_FLAG_FULLSCREEN
 //                or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
 //                or View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
 //                or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
 //                or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION)
-
-        starter.setOnClickListener {
-            val intent = Intent(this@MainActivity, PlayerActivity::class.java)
-            startActivity(intent)
-        }
     }
 }
