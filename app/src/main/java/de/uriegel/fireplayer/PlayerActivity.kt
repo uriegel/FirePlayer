4cf055ae225ed1863d8a9552d527792e6c8ccfaf
Uwe Riegel <uriegel@hotmail.de>
2020-05-09 18:51:28 +0200

    PlayerActivity plays file

--- app/src/main/java/de/uriegel/fireplayer/PlayerActivity.kt
+++ app/src/main/java/de/uriegel/fireplayer/PlayerActivity.kt
@@ -1,8 +1,18 @@
 package de.uriegel.fireplayer
 
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.KeyEvent
 import android.view.View
+import com.google.android.exoplayer2.DefaultLoadControl
+import com.google.android.exoplayer2.DefaultRenderersFactory
+import com.google.android.exoplayer2.ExoPlayer
+import com.google.android.exoplayer2.ExoPlayerFactory
+import com.google.android.exoplayer2.source.ExtractorMediaSource
+import com.google.android.exoplayer2.trackselection.DefaultTrackSelector
+import com.google.android.exoplayer2.upstream.DefaultHttpDataSourceFactory
+import com.google.android.exoplayer2.util.Util
 import kotlinx.android.synthetic.main.activity_player.*
 
 class PlayerActivity : AppCompatActivity() {
@@ -10,12 +20,56 @@ class PlayerActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_player)
-
-//        playerContainer.systemUiVisibility = (View.SYSTEM_UI_FLAG_LOW_PROFILE
-//                or View.SYSTEM_UI_FLAG_FULLSCREEN
-//                or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-//                or View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
-//                or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-//                or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION)
     }
+
+    override fun onStart() {
+        super.onStart()
+        initializePlayer()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        initializePlayer()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        releasePlayer()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        releasePlayer()
+    }
+
+    override fun onKeyDown(keyCode: Int, event: KeyEvent?): Boolean {
+        playerView?.showController()
+        return super.onKeyDown(keyCode, event)
+    }
+
+    private fun initializePlayer() {
+        if (player == null) {
+            player = ExoPlayerFactory.newSimpleInstance(
+                DefaultRenderersFactory(this), DefaultTrackSelector(), DefaultLoadControl()
+            )
+            playerView.player = player
+            player!!.playWhenReady = false
+            //player.seekTo()
+        }
+        val dataSourceFactory = DefaultHttpDataSourceFactory(Util.getUserAgent(this, getString(R.string.app_name)))
+        val mediaSource = ExtractorMediaSource.Factory(dataSourceFactory)
+            .createMediaSource(Uri.parse("https://uriegel.de/video/f3.mp4"))
+        player!!.prepare(mediaSource)
+    }
+
+    private fun releasePlayer() {
+        if (player != null) {
+//            playbackPosition = player.getCurrentPosition()
+//            currentWindow = player.getCurrentWindowIndex()
+            player!!.release()
+            player = null
+        }
+    }
+
+    var player: ExoPlayer? = null
 }
