ede257318f58388867b6b94fed0a1a0aec2169d4
Uwe Riegel <uriegel@hotmail.de>
2020-11-14 13:03:10 +0100

    vue webapp added

---
+++ app/src/main/java/de/uriegel/fireplayer/WebPlayerActivity.kt
@@ -0,0 +1,11 @@
+package de.uriegel.fireplayer
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class WebPlayerActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_web_player)
+    }
+}
