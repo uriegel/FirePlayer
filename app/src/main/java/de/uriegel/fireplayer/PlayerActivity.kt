ca597b495e3f5de5a41f9aa76db2dccd60420bc0
uriegel <uriegel@hotmail.de>
2021-10-02 13:17:31 +0200

    Restore position when rotating view, settings as default action, full screen mode with controls

--- app/src/main/java/de/uriegel/fireplayer/PlayerActivity.kt
+++ app/src/main/java/de/uriegel/fireplayer/PlayerActivity.kt
@@ -2,8 +2,12 @@ package de.uriegel.fireplayer
 
 import android.os.Bundle
 import android.view.KeyEvent
+import android.view.View
 import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.WindowCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.WindowInsetsControllerCompat
 import com.google.android.exoplayer2.*
 import com.google.android.exoplayer2.util.Util
 import de.uriegel.fireplayer.databinding.ActivityPlayerBinding
@@ -21,6 +25,20 @@ class PlayerActivity : AppCompatActivity() {
 
         val intent = intent
         film = intent.getStringExtra("film")!!
+        savedInstanceState?.let {
+            playbackPosition = it.getLong(STATE_PLAYBACK_POSITION, playbackPosition)
+            currentWindow = it.getInt(STATE_CURRENT_WINDOW, currentWindow)
+            playWhenReady = it.getBoolean(STATE_PLAY_WHEN_READY, playWhenReady)
+        }
+
+        viewBinding.playerView.setControllerVisibilityListener {
+            if (it == View.VISIBLE)
+                showSystemUI()
+            else
+                hideSystemUI()
+        }
+
+        hideSystemUI()
     }
 
     override fun onStart() {
@@ -49,7 +67,25 @@ class PlayerActivity : AppCompatActivity() {
 
     override fun onKeyDown(keyCode: Int, event: KeyEvent?): Boolean {
         viewBinding.playerView.showController()
+        if (viewBinding.playerView.isControllerVisible)
+            showSystemUI()
+        else
+            hideSystemUI()
         return super.onKeyDown(keyCode, event)
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putLong(STATE_PLAYBACK_POSITION, playbackPosition)
+        outState.putInt(STATE_CURRENT_WINDOW, currentWindow)
+        outState.putBoolean(STATE_PLAY_WHEN_READY, playWhenReady)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        playbackPosition = savedInstanceState.getLong(STATE_PLAYBACK_POSITION, playbackPosition)
+        currentWindow = savedInstanceState.getInt(STATE_CURRENT_WINDOW, currentWindow)
+        playWhenReady = savedInstanceState.getBoolean(STATE_PLAY_WHEN_READY, playWhenReady)
     }
 
     private fun initializePlayer() {
@@ -78,9 +114,29 @@ class PlayerActivity : AppCompatActivity() {
         player = null
     }
 
-    private val viewBinding by lazy(LazyThreadSafetyMode.NONE) {
+    private fun hideSystemUI() {
+        WindowCompat.setDecorFitsSystemWindows(window, true)
+        WindowInsetsControllerCompat(window, viewBinding.root).let {
+            it.hide(WindowInsetsCompat.Type.systemBars())
+            it.systemBarsBehavior = WindowInsetsControllerCompat.BEHAVIOR_SHOW_BARS_BY_SWIPE
+        }
+    }
+
+    private fun showSystemUI() {
+        WindowCompat.setDecorFitsSystemWindows(window, true)
+        WindowInsetsControllerCompat(window, viewBinding.root).show(WindowInsetsCompat.Type.systemBars())
+    }
+
+    private val viewBinding by lazy {
         ActivityPlayerBinding.inflate(layoutInflater)
     }
+
+    companion object {
+        const val STATE_PLAYBACK_POSITION = "STATE_PLAYBACK_POSITION"
+        const val STATE_CURRENT_WINDOW = "STATE_CURRENT_WINDOW"
+        const val STATE_PLAY_WHEN_READY = "STATE_PLAY_WHEN_READY"
+    }
+
     private var player: SimpleExoPlayer? = null
     private lateinit var film: String
     private var playWhenReady = true
--- app/src/main/java/de/uriegel/fireplayer/VideosAdapter.kt
+++ app/src/main/java/de/uriegel/fireplayer/VideosAdapter.kt
@@ -3,7 +3,7 @@ package de.uriegel.fireplayer
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
+import android.widget.Button
 import androidx.recyclerview.widget.RecyclerView
 
 class VideosAdapter(private val films: Array<String>, private val clickListener: ((track: String)->Unit)) : RecyclerView.Adapter<VideosAdapter.ViewHolder>() {
@@ -20,14 +20,13 @@ class VideosAdapter(private val films: Array<String>, private val clickListener:
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         holder.film = films[position]
         holder.videoNameView.text = holder.film
-        holder.videoNameView.setOnClickListener { holder.clickListener(holder.film) }
     }
 
     class ViewHolder(view: View, val clickListener: ((film: String)->Unit)) : RecyclerView.ViewHolder(view) {
-        val videoNameView: TextView = view.findViewById(R.id.videoNameView)
+        val videoNameView: Button = view.findViewById(R.id.videoNameView)
         var film = ""
         init {
-            view.setOnClickListener {clickListener(film) }
+            videoNameView.setOnClickListener {clickListener(film) }
         }
     }
 }
