d55b596b26b31b4a406d4bfb7bc47b13a35d3726
Uwe Riegel <uriegel@hotmail.de>
2022-12-30 00:00:55 +0100

    MainActivity Compose

---
+++ app/src/main/java/de/uriegel/fireplayer/ui/MainActivity.kt
@@ -0,0 +1,34 @@
+package de.uriegel.fireplayer.ui
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.*
+import androidx.compose.ui.Modifier
+import de.uriegel.fireplayer.ui.theme.FirePlayerTheme
+import de.uriegel.fireplayer.R
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContent {
+            FirePlayerTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    Scaffold(topBar = {
+                        TopAppBar(title = {
+                            Text(getString(R.string.app_title) )
+                        })
+                    }, content = {
+                        MainScreen(it)
+                    })
+                }
+            }
+        }
+    }
+}
---
+++ app/src/main/java/de/uriegel/fireplayer/ui/MainScreen.kt
@@ -0,0 +1,10 @@
+package de.uriegel.fireplayer.ui
+
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun MainScreen(padding: PaddingValues = PaddingValues()) {
+    Text("Test")
+}
