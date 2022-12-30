7933dab02c580fcf54a20eff3829f047f6e2d35c
Uwe Riegel <uriegel@hotmail.de>
2022-12-30 12:10:24 +0100

    KeyDown modifier.onKeyEvent Toast

--- app/src/main/java/de/uriegel/fireplayer/ui/MainActivity.kt
+++ app/src/main/java/de/uriegel/fireplayer/ui/MainActivity.kt
@@ -2,6 +2,8 @@ package de.uriegel.fireplayer.ui
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.KeyEvent
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
@@ -11,6 +13,7 @@ import androidx.compose.material.icons.filled.MoreVert
 import androidx.compose.runtime.*
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.key.onKeyEvent
 import androidx.compose.ui.platform.LocalContext
 import de.uriegel.fireplayer.ui.theme.FirePlayerTheme
 import de.uriegel.fireplayer.R
@@ -18,43 +21,61 @@ import de.uriegel.fireplayer.R
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val isTv =  android.os.Build.MODEL.contains("AFT")
 
         setContent {
+            val context2 = LocalContext.current
             FirePlayerTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
-                    modifier = Modifier.fillMaxSize(),
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .onKeyEvent {
+                            Toast.makeText(context2, "Logout", Toast.LENGTH_LONG).show()
+                            false
+                        },
                     color = MaterialTheme.colors.background
                 ) {
                     var displayMenu by remember { mutableStateOf(false) }
-                    val context = LocalContext.current
-                    Scaffold(topBar = {
-                        TopAppBar(
-                            title = { Text(getString(R.string.app_title) )},
-                            actions = {
-                                IconButton(onClick = { displayMenu = !displayMenu }) {
-                                    Icon(Icons.Default.MoreVert, getString(R.string.menu_settings))
-                                }
-                                DropdownMenu(
-                                    expanded = displayMenu,
-                                    onDismissRequest = { displayMenu = false }
-                                ) {
-
-                                    DropdownMenuItem(onClick = {
-                                        context.startActivity(Intent(context, SettingsActivity::class.java))
-                                        displayMenu = false
-                                    }) {
-                                        Text(text = getString(R.string.menu_settings))
+                    if (!isTv)
+                        Scaffold(topBar = {
+                            TopAppBar(
+                                title = { Text(getString(R.string.app_title) )},
+                                actions = {
+                                    IconButton(onClick = { displayMenu = !displayMenu }) {
+                                        Icon(Icons.Default.MoreVert, getString(R.string.menu_settings))
+                                    }
+                                    DropdownMenu(
+                                        expanded = displayMenu,
+                                        onDismissRequest = { displayMenu = false }
+                                    ) {
+                                        DropdownMenuItem(onClick = {
+                                            showSettings()
+                                            displayMenu = false
+                                        }) {
+                                            Text(text = getString(R.string.menu_settings))
+                                        }
                                     }
                                 }
-                            }
-                        )
-                    }, content = {
-                        MainScreen(it)
-                    })
+                            )
+                        }, content = {
+                            MainScreen(it)
+                        })
+                    else
+                        MainScreen()
                 }
             }
         }
+    }
+
+    override fun onKeyDown(keyCode: Int, event: KeyEvent?): Boolean {
+        if (keyCode == KeyEvent.KEYCODE_MENU)
+            showSettings()
+        return super.onKeyDown(keyCode, event)
+    }
+
+    fun showSettings() {
+        startActivity(Intent(this, SettingsActivity::class.java))
     }
 }
 
