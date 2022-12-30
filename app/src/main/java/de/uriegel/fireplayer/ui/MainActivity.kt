174c4e931a7f636945a7f52392337e5085e06b2c
Uwe Riegel <uriegel@hotmail.de>
2022-12-30 15:18:50 +0100

    LifecycleOwner runs coroutineScope

--- app/src/main/java/de/uriegel/fireplayer/ui/MainActivity.kt
+++ app/src/main/java/de/uriegel/fireplayer/ui/MainActivity.kt
@@ -1,7 +1,10 @@
 package de.uriegel.fireplayer.ui
 
 import android.content.Intent
+import android.net.Uri
+import android.os.Build
 import android.os.Bundle
+import android.provider.Settings
 import android.view.KeyEvent
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -12,12 +15,16 @@ import androidx.compose.material.icons.filled.MoreVert
 import androidx.compose.runtime.*
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalLifecycleOwner
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleEventObserver
 import androidx.preference.PreferenceManager
 import de.uriegel.fireplayer.ui.theme.FirePlayerTheme
 import de.uriegel.fireplayer.R
 import de.uriegel.fireplayer.requests.accessDisk
 import de.uriegel.fireplayer.requests.basicAuthentication
 import de.uriegel.fireplayer.requests.initializeHttp
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -32,6 +39,33 @@ class MainActivity : ComponentActivity() {
                     color = MaterialTheme.colors.background
                 ) {
                     var displayMenu by remember { mutableStateOf(false) }
+
+                    val coroutineScope = rememberCoroutineScope()
+                    val lifecycleOwner = LocalLifecycleOwner.current
+                    DisposableEffect(lifecycleOwner) {
+                        val observer = LifecycleEventObserver { _, event ->
+                            if (event == Lifecycle.Event.ON_RESUME) {
+                                if (urlParts.isEmpty()) {
+                                    initializeHttp(this@MainActivity)
+                                    urlParts = arrayOf("/video")
+                                }
+                                if (urlParts.isEmpty())
+                                    showSettings()
+                                coroutineScope.launch {
+                                    accessDisk()
+                                }
+                                // TODO if no connection screen with text check connection and one button "settings"
+                                // TODO if connection and response error "settings"
+                            }
+                        }
+
+                        lifecycleOwner.lifecycle.addObserver(observer)
+
+                        onDispose {
+                            lifecycleOwner.lifecycle.removeObserver(observer)
+                        }
+                    }
+
                     if (!isTv)
                         Scaffold(topBar = {
                             TopAppBar(
@@ -61,26 +95,6 @@ class MainActivity : ComponentActivity() {
                 }
             }
         }
-    }
-
-    // TODO Lifecyclemanager with coroutine scope
-    // var corutimSkop = rememberCoroutineScope()
-
-    //                    Lifecycle.Event.ON_START -> {
-    //                        corutimSkop.launch {
-    override fun onResume() {
-        super.onResume()
-
-        if (urlParts.isEmpty()) {
-            initializeHttp(this@MainActivity)
-            urlParts = arrayOf("/video")
-        }
-        if (urlParts.isEmpty())
-            showSettings()
-
-        accessDisk()
-        // TODO if no connection screen with text check connection and one button "settings"
-        // TODO if connection and response error "settings"
     }
 
     override fun onKeyDown(keyCode: Int, event: KeyEvent?): Boolean {
