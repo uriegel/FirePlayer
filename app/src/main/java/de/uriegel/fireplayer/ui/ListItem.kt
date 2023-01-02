3534f3df80f602cd15e08cdd57b872bc2aab06d6
Uwe Riegel <uriegel@hotmail.de>
2023-01-02 03:17:03 +0100

    ListItem styled

--- app/src/main/java/de/uriegel/fireplayer/ui/ListItem.kt
+++ app/src/main/java/de/uriegel/fireplayer/ui/ListItem.kt
@@ -14,59 +14,40 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import de.uriegel.fireplayer.R
-import de.uriegel.fireplayer.extensions.getTitle
 import de.uriegel.fireplayer.extensions.isFolder
 import de.uriegel.fireplayer.ui.theme.FirePlayerTheme
 import de.uriegel.fireplayer.ui.theme.card
 
 @Composable
-fun FolderItem(item: String, modifier: Modifier = Modifier) {
-    Card(
-        shape = RoundedCornerShape(10.dp),
-        elevation=5.dp,
-        modifier = modifier.padding(5.dp)
-    ){
-        Box(
-            modifier = Modifier
-                .background(card)
+fun ListItem(item: String, modifier: Modifier = Modifier) {
+
+    val isFolder = item.isFolder()
+    val boxModifier =
+        if (isFolder)
+            Modifier
                 .paint(
                     painterResource(id = R.drawable.folder),
-                    contentScale = ContentScale.FillBounds
-                )
+                    contentScale = ContentScale.FillBounds)
                 .padding(12.dp)
-                .aspectRatio(1.8f),
-            contentAlignment = Alignment.Center
-        ) {
-            Text(item)
-        }
-    }
-}
+        else
+            Modifier
+                .padding(5.dp)
 
-@Composable
-fun Item(item: String, modifier: Modifier = Modifier) {
     Card(
         shape = RoundedCornerShape(10.dp),
         elevation=5.dp,
-        modifier = modifier
-            .aspectRatio(1.8f)
-            .padding(5.dp)
+        modifier =
+            modifier
+                .aspectRatio(1.8f)
+                .padding(5.dp)
     ) {
-        Box(modifier = Modifier
-            .background(card)
-            .padding(5.dp),
+        Box(
+            modifier = boxModifier.background(card),
             contentAlignment = Alignment.Center
         ) {
             Text(item)
         }
     }
-}
-
-@Composable
-fun ListItem(item: String, modifier: Modifier = Modifier) {
-    if (item.isFolder())
-        FolderItem(item, modifier)
-    else
-        Item(item.getTitle(), modifier)
 }
 
 @Composable
