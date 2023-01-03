7572a2b0c215a5beda0083ddc8dcd060a556f3f5
Uwe Riegel <uriegel@hotmail.de>
2023-01-03 13:00:04 +0100

    exoPlayer was created 2 times!

--- app/src/main/java/de/uriegel/fireplayer/ui/VideoScreen.kt
+++ app/src/main/java/de/uriegel/fireplayer/ui/VideoScreen.kt
@@ -4,12 +4,10 @@ import android.view.View
 import android.view.WindowManager
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.DisposableEffect
-import androidx.compose.runtime.MutableState
-import androidx.compose.runtime.rememberUpdatedState
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.key.onKeyEvent
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalLifecycleOwner
 import androidx.compose.ui.viewinterop.AndroidView
@@ -29,7 +27,8 @@ import de.uriegel.fireplayer.requests.getBaseUrl
 fun VideoScreen(fullscreenMode: MutableState<Boolean>, path64: String?) {
     Box(
         contentAlignment = Alignment.Center,
-        modifier = Modifier.fillMaxSize()
+        modifier = Modifier
+            .fillMaxSize()
     ) {
         VideoPlayer(fullscreenMode, path64)
     }
@@ -40,23 +39,29 @@ fun VideoPlayer(fullscreenMode: MutableState<Boolean>, path64: String?) {
     val context = LocalContext.current
     val path = path64!!.fromBase64()
 
-    val exoPlayer = ExoPlayer.Builder(context)
-        .build()
-        .also {
-            val mediaItem = MediaItem
-                .Builder()
-                .setUri(getBaseUrl() + path)
-                .build()
-            it.setMediaItem(mediaItem)
-            it.prepare()
-        }
-
+    val exoPlayer: MutableState<ExoPlayer?> = remember  { mutableStateOf(null) }
     val lifecycleOwner = rememberUpdatedState(LocalLifecycleOwner.current)
 
     DisposableEffect(
         AndroidView(factory = {
+            exoPlayer.value = ExoPlayer.Builder(context)
+                .build()
+                .also {
+                    val mediaItem = MediaItem
+                        .Builder()
+                        .setUri(getBaseUrl() + path)
+                        .build()
+                    it.setMediaItem(mediaItem)
+                    it.prepare()
+                }
+
             StyledPlayerView(context).apply {
-                player = exoPlayer
+                player = exoPlayer.value
+                this.setOnKeyListener { _, _, _ ->
+                   showController()
+                   false
+                }
+
                 setControllerVisibilityListener(StyledPlayerView.ControllerVisibilityListener {
                     if (it == View.VISIBLE)
                         context.showSystemUi()
@@ -71,12 +76,12 @@ fun VideoPlayer(fullscreenMode: MutableState<Boolean>, path64: String?) {
                 Lifecycle.Event.ON_RESUME -> {
                     fullscreenMode.value = true
                     context.addWindowFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
-                    exoPlayer.play()
+                    exoPlayer.value?.play()
                 }
                 Lifecycle.Event.ON_PAUSE -> {
                     fullscreenMode.value = false
                     context.clearWindowFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
-                    exoPlayer.pause()
+                    exoPlayer.value?.pause()
                 }
                 else -> {}
             }
@@ -84,6 +89,9 @@ fun VideoPlayer(fullscreenMode: MutableState<Boolean>, path64: String?) {
         val lifecycle = lifecycleOwner.value.lifecycle
         lifecycle.addObserver(observer)
 
-        onDispose { exoPlayer.release() }
+        onDispose {
+            exoPlayer.value?.release()
+            exoPlayer.value = null
+        }
     }
 }
