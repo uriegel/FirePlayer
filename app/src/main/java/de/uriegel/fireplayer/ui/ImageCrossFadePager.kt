b2217e88635d695de84c27a1053399b91d519096
Uwe Riegel <uriegel@hotmail.de>
2023-01-15 21:32:42 +0100

    ImageFadePager crossfades between 2 images now OK forward

--- app/src/main/java/de/uriegel/fireplayer/ui/ImageCrossFadePager.kt
+++ app/src/main/java/de/uriegel/fireplayer/ui/ImageCrossFadePager.kt
@@ -1,9 +1,11 @@
 package de.uriegel.fireplayer.ui
 
 import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import androidx.compose.animation.core.tween
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.Button
 import androidx.compose.material.Text
@@ -11,6 +13,9 @@ import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.graphics.asImageBitmap
+import androidx.compose.ui.platform.LocalContext
+import de.uriegel.fireplayer.R
 import kotlinx.coroutines.launch
 
 @Composable
@@ -18,54 +23,85 @@ fun ImageCrossFadePager(
     count: Int,
     loadAsync: suspend (Int)-> Bitmap?
 ) {
+    val context = LocalContext.current
     val scope = rememberCoroutineScope()
-    var index by remember { mutableStateOf(0) }
-    val alpha = remember { androidx.compose.animation.core.Animatable(0F) }
+    var secondVisible by remember { mutableStateOf(false)}
+    val alpha = remember { androidx.compose.animation.core.Animatable(1F) }
+    var index by remember { mutableStateOf(0)}
+    var bitmap1: Bitmap? by remember { mutableStateOf(null)}
+    var bitmap2: Bitmap? by remember { mutableStateOf(null)}
+    var bitmapNext: Bitmap? by remember { mutableStateOf(null)}
 
-    LaunchedEffect(index) {
+    LaunchedEffect(true) {
+        scope.launch {
+            loadAsync(0)?.let {
+                bitmap1 = it
+            }
+            loadAsync(1)?.let {
+                bitmapNext = it
+            }
+        }
+
+    }
+
+    LaunchedEffect(secondVisible) {
         alpha.animateTo(
-            when (index.mod(3)) {
-                0 -> 0F
-                1 -> 1F
-                2 -> 2F
-                else -> 0F
+            when (secondVisible) {
+                false -> 1F
+                true -> 0F
             },
-            animationSpec = tween(2000))
+            animationSpec = tween(1000))
     }
 
     Box(modifier =  Modifier.fillMaxSize()) {
-        AsyncImage(
+        Image(
             modifier = Modifier
                 .align(Alignment.Center)
-                .alpha(if (index.mod(3) == 0 || index.mod(3) == 1) 1f - alpha.value else 0F),
+                .alpha(alpha.value),
+            bitmap = bitmap1?.asImageBitmap()
+                ?: BitmapFactory.decodeResource(context.resources,
+                    R.drawable.emptypics).asImageBitmap(),
             contentDescription = "Image",
-            loadAsync = { loadAsync(0) }
         )
-        AsyncImage(
+        Image(
             modifier = Modifier
                 .align(Alignment.Center)
-                .alpha(if (index.mod(3) == 1) alpha.value else if (index.mod(3) == 2) 2f - alpha.value else 0f),
+                .alpha(1f - alpha.value),
+            bitmap = bitmap2?.asImageBitmap()
+                ?: BitmapFactory.decodeResource(context.resources,
+                    R.drawable.emptypics).asImageBitmap(),
             contentDescription = "Image",
-            loadAsync = { loadAsync(1) }
         )
-        AsyncImage(
-            modifier = Modifier
-                .align(Alignment.Center)
-                .alpha(if (index.mod(3) == 2) alpha.value - 1f else if (index.mod(3) == 0) alpha.value else 0f),
-            contentDescription = "Image",
-            loadAsync = { loadAsync(2) }
-        )
-        Column {
+        Row {
             Button({
-                scope.launch {
-                    index--
+                if (index != 0) {
+                    if (secondVisible)
+                        bitmap1 = bitmapNext
+                    else
+                        bitmap2 = bitmapNext
+                    scope.launch {
+                        secondVisible = !secondVisible
+                        loadAsync(--index + 1)?.let {
+                            bitmapNext = it
+                        }
+                    }
                 }
             }) {
                 Text("Zurück")
             }
             Button({
-                scope.launch {
-                    index++
+                if (index < count - 1) {
+                    if (secondVisible)
+                        bitmap1 = bitmapNext
+                    else
+                        bitmap2 = bitmapNext
+                    scope.launch {
+                        secondVisible = !secondVisible
+                        if (index < count - 2)
+                            loadAsync(++index + 1)?.let {
+                                bitmapNext = it
+                            }
+                    }
                 }
             }) {
                 Text("Weiter")
@@ -73,3 +109,6 @@ fun ImageCrossFadePager(
         }
     }
 }
+
+
+
