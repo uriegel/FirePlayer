336d58230dd76cb7b68086c0559c33f75289c4b6
Uwe Riegel <uriegel@hotmail.de>
2023-10-13 22:42:56 +0200

    Rotation on TV with fluent animation

--- app/src/main/java/de/uriegel/fireplayer/ui/ImagePager.kt
+++ app/src/main/java/de/uriegel/fireplayer/ui/ImagePager.kt
@@ -1,60 +1,45 @@
 package de.uriegel.fireplayer.ui
 
-import android.content.Context
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
-import android.util.Log
 import android.view.KeyEvent
 import androidx.compose.animation.core.tween
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.gestures.Orientation
-import androidx.compose.foundation.gestures.draggable
-import androidx.compose.foundation.gestures.rememberDraggableState
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableIntStateOf
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberCoroutineScope
-import androidx.compose.runtime.setValue
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
-import androidx.compose.ui.draw.rotate
-import androidx.compose.ui.draw.scale
 import androidx.compose.ui.graphics.asImageBitmap
 import androidx.compose.ui.platform.LocalContext
-import androidx.exifinterface.media.ExifInterface
 import de.uriegel.fireplayer.R
 import de.uriegel.fireplayer.extensions.onKeyDown
 import kotlinx.coroutines.launch
 
 @Composable
-fun ImagePager(
+fun ImageCrossFadePager(
     count: Int,
-    loadAsync: suspend (Int)-> ByteArray?,
+    loadAsync: suspend (Int)-> Bitmap?
 ) {
-    val scope = rememberCoroutineScope()
     val context = LocalContext.current
-    var index by remember { mutableIntStateOf(0) }
-    var loading by remember { mutableStateOf(false)}
-    var imageData1: ImageData? by remember { mutableStateOf(null)}
-    var imageData2: ImageData? by remember { mutableStateOf(null)}
-    var imageDataNext: ImageData? by remember { mutableStateOf(null)}
-    var imageDataPrev: ImageData? by remember { mutableStateOf(null)}
+    val scope = rememberCoroutineScope()
     var secondVisible by remember { mutableStateOf(false)}
     val alpha = remember { androidx.compose.animation.core.Animatable(1F) }
+    var index by remember { mutableStateOf(0)}
+    var loading by remember { mutableStateOf(false)}
+    var bitmap1: Bitmap? by remember { mutableStateOf(null)}
+    var bitmap2: Bitmap? by remember { mutableStateOf(null)}
+    var bitmapNext: Bitmap? by remember { mutableStateOf(null)}
+    var bitmapPrev: Bitmap? by remember { mutableStateOf(null)}
 
     LaunchedEffect(true) {
         scope.launch {
             loadAsync(0)?.let {
-                imageData1 = loadImageData(it)
+                bitmap1 = it
             }
             loadAsync(1)?.let {
-                imageDataNext = loadImageData(it)
+                bitmapNext = it
             }
         }
 
@@ -66,129 +51,79 @@ fun ImagePager(
                 false -> 1F
                 true -> 0F
             },
-            animationSpec = tween(1000)
-        )
+            animationSpec = tween(1000))
     }
 
-    fun next() {
-        if (!loading && index < count - 1) {
-            if (secondVisible) {
-                imageData1 = imageDataNext
-                imageDataPrev = imageData2
-            } else {
-                imageData2 = imageDataNext
-                imageDataPrev = imageData1
-            }
-            loading = true
-            scope.launch {
-                secondVisible = !secondVisible
-                if (index++ < count - 2)
-                    loadAsync(index + 1)?.let {
-                        imageDataNext = loadImageData(it)
-                        loading = false
-                    } else
-                    loading = false
-            }
-        }
-    }
-
-    fun previous() {
-        if (!loading && index != 0) {
-            if (secondVisible) {
-                imageData1 = imageDataPrev
-                imageDataNext = imageData2
-            } else {
-                imageData2 = imageDataPrev
-                imageDataNext = imageData1
-            }
-            loading = true
-            scope.launch {
-                secondVisible = !secondVisible
-                if (index-- > 1)
-                    loadAsync(index - 1)?.let {
-                        imageDataPrev = loadImageData(it)
-                        loading = false
-                    } else
-                    loading = false
-            }
-        }
-    }
-
-    Box(modifier = Modifier
+    Box(modifier =  Modifier
         .fillMaxSize()
-        .draggable(
-            orientation = Orientation.Horizontal,
-            state = rememberDraggableState { delta ->
-                if (delta > -20 && delta < 0)
-                    next()
-                else if (delta < 20 && delta > 0)
-                    previous()
-            }
-        )
         .onKeyDown(context) { _, evt ->
             when (evt?.keyCode) {
                 KeyEvent.KEYCODE_DPAD_RIGHT -> {
-                    next()
+                    if (!loading && index < count - 1) {
+                        if (secondVisible) {
+                            bitmap1 = bitmapNext
+                            bitmapPrev = bitmap2
+                        } else {
+                            bitmap2 = bitmapNext
+                            bitmapPrev = bitmap1
+                        }
+                        loading = true
+                        scope.launch {
+                            secondVisible = !secondVisible
+                            if (index++ < count - 2)
+                                loadAsync(index + 1)?.let {
+                                    bitmapNext = it
+                                    loading = false
+                                } else
+                                loading = false
+                        }
+                    }
                     true
                 }
                 KeyEvent.KEYCODE_DPAD_LEFT -> {
-                    previous()
+                    if (!loading && index != 0) {
+                        if (secondVisible) {
+                            bitmap1 = bitmapPrev
+                            bitmapNext = bitmap2
+                        } else {
+                            bitmap2 = bitmapPrev
+                            bitmapNext = bitmap1
+                        }
+                        loading = true
+                        scope.launch {
+                            secondVisible = !secondVisible
+                            if (index-- > 1)
+                                loadAsync(index - 1)?.let {
+                                    bitmapPrev = it
+                                    loading = false
+                                } else
+                                loading = false
+
+                        }
+                    }
                     true
                 }
                 else -> false
             }
         }
     ) {
-        AsyncImage(context, Modifier.align(Alignment.Center), imageData1, alpha.value)
-        AsyncImage(context, Modifier.align(Alignment.Center), imageData2, 1f - alpha.value)
-    }
-}
-
-@Composable
-fun AsyncImage(context: Context, modifier: Modifier, imageData: ImageData?, alpha: Float) {
-    Image(
-        modifier = modifier
-            .then(
-                if (imageData?.angle != 0f) {
-                    Modifier
-                        .rotate(imageData?.angle ?: 0f)
-                        .scale(
-                            (imageData?.bitmap?.height?.toFloat()
-                                ?: 1f) / (imageData?.bitmap?.width?.toFloat() ?: 1f)
-                        )
-                } else
-                    Modifier
-            )
-            .alpha(alpha),
-        bitmap = imageData?.bitmap?.asImageBitmap()
-            ?: BitmapFactory.decodeResource(
-                context.resources,
-                R.drawable.emptypics
-            ).asImageBitmap(),
-        contentDescription = "Image"
-    )
-}
-
-data class ImageData(
-    val bitmap: Bitmap,
-    val angle: Float
-)
-
-fun loadImageData(bitmapBytes: ByteArray): ImageData {
-    val angle = bitmapBytes.inputStream().use {
-        val exif = ExifInterface(it)
-        val orientation = exif.getAttributeInt(
-            ExifInterface.TAG_ORIENTATION,
-            ExifInterface.ORIENTATION_NORMAL
+        Image(
+            modifier = Modifier
+                .align(Alignment.Center)
+                .alpha(alpha.value),
+            bitmap = bitmap1?.asImageBitmap()
+                ?: BitmapFactory.decodeResource(context.resources,
+                    R.drawable.emptypics).asImageBitmap(),
+            contentDescription = "Image",
         )
-        when (orientation) {
-            ExifInterface.ORIENTATION_ROTATE_90 -> 90f
-            ExifInterface.ORIENTATION_ROTATE_270 -> 270f
-            ExifInterface.ORIENTATION_ROTATE_180 -> 180f
-            else -> 0f
-        }
+        Image(
+            modifier = Modifier
+                .align(Alignment.Center)
+                .alpha(1f - alpha.value),
+            bitmap = bitmap2?.asImageBitmap()
+                ?: BitmapFactory.decodeResource(context.resources,
+                    R.drawable.emptypics).asImageBitmap(),
+            contentDescription = "Image",
+        )
     }
-    Log.i("FOTO", "Winkel $angle")
-    return ImageData(BitmapFactory.decodeByteArray(bitmapBytes, 0, bitmapBytes.size), angle)
 }
-
--- app/src/main/java/de/uriegel/fireplayer/ui/PhotoScreen.kt
+++ app/src/main/java/de/uriegel/fireplayer/ui/PhotoScreen.kt
@@ -1,6 +1,23 @@
 package de.uriegel.fireplayer.ui
 
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.graphics.Matrix
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.rotate
+import androidx.compose.ui.graphics.asImageBitmap
+import androidx.compose.ui.platform.LocalContext
+import androidx.exifinterface.media.ExifInterface
+import com.google.accompanist.pager.ExperimentalPagerApi
+import com.google.accompanist.pager.HorizontalPager
+import com.google.accompanist.pager.rememberPagerState
+import de.uriegel.fireplayer.R
+import de.uriegel.fireplayer.android.isTv
 import de.uriegel.fireplayer.extensions.fromBase64
 import de.uriegel.fireplayer.extensions.getFilePath
 import de.uriegel.fireplayer.extensions.isPicture
@@ -8,7 +25,10 @@ import de.uriegel.fireplayer.extensions.readAll
 import de.uriegel.fireplayer.requests.getResponseStream
 import de.uriegel.fireplayer.viewmodel.DirectoryItemsViewModel
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
+import java.util.*
+
 
 @Composable
 fun PhotoScreen(viewModel: DirectoryItemsViewModel, path64: String?) {
@@ -17,19 +37,96 @@ fun PhotoScreen(viewModel: DirectoryItemsViewModel, path64: String?) {
     val items = viewModel.items
         .filter { it.name.isPicture() }
         .map { (filePath + it.name).replace("+", "%20") }
-    ImagePager(
-        count = items.size,
-        loadAsync = { loadBitmap(items[it]) },
+    if (isTv())
+        ImageCrossFadePager(
+            count = items.size,
+            loadAsync = { loadBitmap(items[it]) }
+        )
+    else
+        ImagePager(
+            count = items.size,
+            loadAsync = { loadBitmap(items[it]) }
+        )
+}
+
+@OptIn(ExperimentalPagerApi::class)
+@Composable
+fun ImagePager(
+    count: Int,
+    loadAsync: suspend (Int)-> Bitmap?
+) {
+    val pagerState = rememberPagerState()
+    HorizontalPager(count = count, state = pagerState) { pager ->
+        Box(Modifier
+            .fillMaxSize()) {
+            AsyncImage(
+                modifier = Modifier.align(Alignment.Center),
+                contentDescription = "Image",
+                loadAsync = { loadAsync(pager) }
+            )
+        }
+    }
+}
+
+@Composable
+fun AsyncImage(
+    modifier: Modifier = Modifier,
+    loadAsync: suspend ()-> Bitmap?,
+    contentDescription: String,
+) {
+    val context = LocalContext.current
+    val scope = rememberCoroutineScope()
+    val bitmap = remember {
+        mutableStateOf(BitmapFactory.decodeResource(context.resources, R.drawable.emptypics))
+    }
+
+    LaunchedEffect(true) {
+        scope.launch {
+            loadAsync()?.apply {
+                bitmap.value = this
+            }
+        }
+    }
+
+    Image(
+        modifier = modifier.rotate(90f),
+        bitmap = bitmap.value.asImageBitmap(),
+        contentDescription = contentDescription,
     )
 }
 
-suspend fun loadBitmap(url: String): ByteArray? =
+suspend fun loadBitmap(url: String): Bitmap? =
     withContext(Dispatchers.IO) {
         return@withContext getResponseStream(url)
             .map {
                 it.readAll()
             }
-            .getOrNull()
+            .fold({
+                val degree = it.inputStream().use { bytes ->
+                    val exif = ExifInterface(bytes)
+                    val orientation = exif.getAttributeInt(
+                        ExifInterface.TAG_ORIENTATION,
+                        ExifInterface.ORIENTATION_NORMAL
+                    )
+                    if (orientation == ExifInterface.ORIENTATION_ROTATE_90)
+                        90f
+                    else if (orientation == ExifInterface.ORIENTATION_ROTATE_270)
+                        270f
+                    else if (orientation == ExifInterface.ORIENTATION_ROTATE_180)
+                        180f
+                    else
+                        0f
+                }
+                rotateImage(BitmapFactory.decodeByteArray(it, 0, it.size), degree)
+            }, { null })
     }
 
-
+private fun rotateImage(bitmap: Bitmap, angle: Float): Bitmap {
+    return if (angle == 0f)
+        bitmap
+    else {
+        val mat = Matrix()
+        mat.postRotate(angle)
+        Bitmap.createBitmap(bitmap, 0, 0, bitmap.width, bitmap.height, mat, true)
+    }
+}
