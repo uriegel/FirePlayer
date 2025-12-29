bbc662fea98a3e7b75bdd12b5320152a0d354609
Uwe Riegel <uriegel@hotmail.de>
2025-12-29 09:57:20 +0100

    Ripple animation for Firestick too

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -13,8 +13,8 @@ export default function App() {
 
 	return (
 		<div className='viewer'>
-			<button className="ripple-container" onPointerDown={ripple} ref={fokusButton} onClick={() => onClick("Button 1")}>Erste Aktion</button>
-			<div className="ripple-container" onPointerDown={ripple} tabIndex={0} onClick={() => onClick("Button 2")}>Zweite Aktion</div>
+			<button className="ripple-container" onPointerDown={ripple.onPointerDown} onKeyDown={ripple.onKeyDown} ref={fokusButton} onClick={() => onClick("Button 1")}>Erste Aktion</button>
+			<div className="ripple-container" onPointerDown={ripple.onPointerDown} onKeyDown={ripple.onKeyDown} tabIndex={0} onClick={() => onClick("Button 2")}>Zweite Aktion</div>
 			<button onClick={() => onClick("Button 3")}>Dritte Aktion</button>
         	<video className='mediaPlayer' controls autoPlay src="http://roxy:9865/video/2010.mp4" />        
 		</div>
--- web/src/hooks/ripple.tsx
+++ web/src/hooks/ripple.tsx
@@ -1,21 +1,31 @@
-import { useCallback } from "react"
-
 export function useRipple() {
-    return useCallback(
-        (e: React.PointerEvent<HTMLElement>) => {
+    const createRipple = (target: HTMLElement, x: number, y: number) => {
+        const rect = target.getBoundingClientRect()
+        const ripple = document.createElement("span")
+        const size = Math.max(rect.width, rect.height)
+
+        ripple.className = "ripple"
+        ripple.style.width = ripple.style.height = `${size}px`
+        ripple.style.left = `${x - size / 2}px`
+        ripple.style.top = `${y - size / 2}px`
+
+        target.appendChild(ripple)
+        ripple.addEventListener("animationend", () => ripple.remove())
+    }
+
+    return {
+        onPointerDown: (e: React.PointerEvent<HTMLElement>) => {
+            const rect = e.currentTarget.getBoundingClientRect()
+            createRipple(e.currentTarget, e.clientX - rect.left, e.clientY - rect.top )
+        },
+        onKeyDown: (e: React.KeyboardEvent<HTMLElement>) => {
+            if (e.key !== "Enter" && e.key !== " ")
+                return
+
             const target = e.currentTarget
             const rect = target.getBoundingClientRect()
 
-            const ripple = document.createElement("span")
-            const size = Math.max(rect.width, rect.height)
-
-            ripple.className = "ripple"
-            ripple.style.width = ripple.style.height = `${size}px`
-            ripple.style.left = `${e.clientX - rect.left - size / 2}px`
-            ripple.style.top = `${e.clientY - rect.top - size / 2}px`
-
-            target.appendChild(ripple)
-
-            ripple.addEventListener("animationend", () => ripple.remove())
-        }, [])
+            createRipple(target, rect.width / 2, rect.height / 2 )
+        }
+    }
 }
